b9256ff3ce6459272ca589629033fd397a8988df
Benjamin Nwabunwanne <benluvforu@gmail.com>
2026-06-07 13:01:40 +0100

    add locale check funtionalities

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const country = req.geo?.country || req.headers.get('x-vercel-ip-country') || 'NG';
+  const country = req.geo?.country || req.headers.get('x-vercel-ip-country') || 'US';
 
   const res = NextResponse.next();
 
